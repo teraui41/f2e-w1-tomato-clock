42b8bcb2e301244695cfd83898af62bbbb17a28a
Edmond Wu <edmond.wu@velocitylight.net>
2019-07-19 13:03:07 +0800

    ADD

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,5 +1,5 @@
-export const INIT_DAY_TIME = 25 * 1000;
+export const INIT_DAY_TIME = 25 * 60 * 1000;
 
-export const INIT_NIGHT_TIME = 5 * 1000;
+export const INIT_NIGHT_TIME = 5 * 60 * 1000;
 
-export const EVENING_TIME = 5 * 1000;
+export const EVENING_TIME = 5 * 60 * 1000;
